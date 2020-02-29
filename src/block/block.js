2d6e0a61dfd95ac01e4ab7f276a683431ea3e344
Edward Munoz <edward.munoz@pcel.com>
2020-02-28 18:45:42 -0600

    beta

--- src/block/block.js
+++ src/block/block.js
@@ -13,91 +13,88 @@ registerBlockType("cgb/block-decluster", {
 	category: "widgets",
 	keywords: ["decluster", "emunoz"],
 	attributes: {
+		// cards: {
+		// 	source: "query",
+		// 	default: [],
+		// 	selector: ".card",
+		// 	query: {
+		// 		title: {
+		// 			type: "string",
+		// 			source: "text",
+		// 			selector: "h3"
+		// 		}
+		// 	}
+		// },
+
 		cards: {
-			source: "query",
-			default: [],
-			selector: ".card",
-			query: {
-				title: {
-					type: "string",
-					source: "text",
-					selector: "h3"
-				}
-			}
+			type: "array",
+			default: []
 		},
 
 		number_cards: {
 			type: "number",
 			default: 1
-		},
-		imageAlt: {
-			attribute: "alt",
-			selector: ".card__image"
-		},
-		imageUrl: {
-			attribute: "src",
-			selector: ".card__image"
 		}
+		// imageAlt: {
+		// 	attribute: "alt",
+		// 	selector: ".card__image"
+		// },
+		// imageUrl: {
+		// 	attribute: "src",
+		// 	selector: ".card__image"
+		// }
 	},
 
 	edit: ({ attributes, className, setAttributes }) => {
-		const { cards } = attributes;
+		const handleRangeControl = content => {
+			setAttributes({ number_cards: content });
 
-		console.log(cards);
+			const cards = [...attributes.cards];
 
-		function onChangeTitle() {
-			// setAttributes({ number_cards: content });
-		}
+			for (let i = 0; i < content; i++) {
+				if (cards.length < content) {
+					cards.push({
+						title: ""
+					});
+				}
+				if (cards.length > content) {
+					cards.pop();
+				}
 
-		const displayCards = cards => {
-			return (
-				//Loops throug the cards
-				cards.map((card, i) => {
-					return (
-						<div class="card">
-							<div class="card-container"></div>
+				if (cards == content) {
+					break;
+				}
+			}
 
-							<div class="card-info same">
-								<div class="card-title">
-									<RichText
-										tagName="h3"
-										placeholder={"Titulo " + i}
-										value={card.title}
-										onChange={e => {
-											// var newObject = Object.assign({}, card, {
-											// 	title: e
-											// });
-											// var attrs = {};
-											// attrs[key] = e;
-											// props.setAttributes(attrs);
-											const newItem = { ...card, title: e };
-											const newItems = [...cards];
-											newItems[i] = newItem;
-											setAttributes({ cards: newItems });
-
-											// return setAttributes({
-											// 	cards: [].concat(
-											// 		_cloneArray(
-											// 			attributes.cards.filter(function(cardFilter) {
-											// 				return cardFilter.index != card.index;
-											// 			})
-											// 		),
-											// 		[card]
-											// 	)
-											// });
-										}}
-									/>
-								</div>
-							</div>
-
-							<div class="card-btn" onClick={() => console.log(attributes)}>
-								Ver productos
-							</div>
-						</div>
-					);
-				})
-			);
+			setAttributes({ cards });
 		};
+
+		const handleTitleChange = (title, index) => {
+			const cards = [...attributes.cards];
+			cards[index].title = title;
+			setAttributes({ cards });
+		};
+
+		let cardViewDisplay;
+		// const { cards } = attributes;
+
+		// console.log(cards);
+
+		// function onChangeTitle() {
+		// setAttributes({ number_cards: content });
+		// }
+
+		// const displayCards = cards => {
+		// 	return (
+		// 		//Loops throug the cards
+		// 		cards.map((card, i) => {
+		// 			return (
+		//
+		// 			);
+		// 		})
+		// 	);
+		// };
+
 		//
 		// 		<div class="card-image">
 		// 			{/*
@@ -129,7 +126,7 @@ registerBlockType("cgb/block-decluster", {
 		// 				className="card-text"
 		// 				tagName="p"
 		// 				placeholder={"Ingrese una descripcion "}
-		// 				// value={props.attributes.desc}
+		// 				// value={attributes.desc}
 		// 				// onChange={content => setAttributes({ desc })}
 		// 			/> */}
 		// 		</div>
@@ -140,79 +137,77 @@ registerBlockType("cgb/block-decluster", {
 		// 	</div>
 		// </div>
 
-		function _cloneArray(arr) {
-			if (Array.isArray(arr)) {
-				for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
-					arr2[i] = arr[i];
-				}
-				return arr2;
-			} else {
-				return Array.from(arr);
-			}
-		}
+		// function _cloneArray(arr) {
+		// 	if (Array.isArray(arr)) {
+		// 		for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
+		// 			arr2[i] = arr[i];
+		// 		}
+		// 		return arr2;
+		// 	} else {
+		// 		return Array.from(arr);
+		// 	}
+		// }
 
-		function onChangeRangeControl(content) {
-			setAttributes({ number_cards: content });
+		// 	setAttributes({ cards: cards });
 
-			// setAttributes({ cards: Array(content).fill({ title: "", desc: "" }) });
+		// 	// console.log(cards);
+		// }
 
-			// console.log("number_cards " + content);
-			let cards = attributes.cards;
-			for (let i = 0; i < content; i++) {
-				if (cards.length < content) {
-					cards.push({
-						title: ""
-					});
-				}
-				if (cards.length > content) {
-					cards.pop();
-				}
+		// const getImageButton = openEvent => {
+		// 	if (attributes.imageUrl) {
+		// 		return (
+		// 			<img
+		// 				src={attributes.imageUrl}
+		// 				onClick={openEvent}
+		// 				className="image"
+		// 			/>
+		// 		);
+		// 	} else {
+		// 		return (
+		// 			<div className="button-container">
+		// 				<Button onClick={openEvent} className="button button-large">
+		// 					Pick an image
+		// 				</Button>
+		// 			</div>
+		// 		);
+		// 	}
+		// };
 
-				if (cards == content) {
-					break;
-				}
-			}
-
-			setAttributes({ cards: cards });
-
-			// console.log(cards);
-		}
-
-		const getImageButton = openEvent => {
-			if (attributes.imageUrl) {
+		if (attributes.cards.length) {
+			cardViewDisplay = attributes.cards.map((location, index) => {
 				return (
-					<img
-						src={attributes.imageUrl}
-						onClick={openEvent}
-						className="image"
-					/>
+					<Fragment key={index}>
+						<div class="card">
+							<div class="card-container"></div>
+
+							<div class="card-info same">
+								<div class="card-title">
+									<RichText
+										tagName="h3"
+										placeholder={"Titulo " + index}
+										value={attributes.cards[index].title}
+										onChange={title => handleTitleChange(title, index)}
+									/>
+								</div>
+							</div>
+
+							<div class="card-btn" onClick={() => console.log(attributes)}>
+								Ver productos
+							</div>
+						</div>
+					</Fragment>
 				);
-			} else {
-				return (
-					<div className="button-container">
-						<Button onClick={openEvent} className="button button-large">
-							Pick an image
-						</Button>
-					</div>
-				);
-			}
-		};
+			});
+		}
 
 		return (
 			<Fragment>
-				<div class="album album-home text-muted">
-					<div class="row">
-						<h1>1</h1>
-						{displayCards(attributes.cards)}
-					</div>
-				</div>
-
-				<InspectorControls>
+				<InspectorControls key="1">
 					<PanelBody title="Form Settings" initialOpen={true}>
 						<PanelRow>
 							<RangeControl
 								value={attributes.number_cards}
-								onChange={onChangeRangeControl}
+								onChange={handleRangeControl}
 								min={1}
 								max={10}
 								step={1}
@@ -220,32 +215,24 @@ registerBlockType("cgb/block-decluster", {
 						</PanelRow>
 					</PanelBody>
 				</InspectorControls>
+
+				<div key="2" className={className + " album album-home text-muted"}>
+					<div class="row">{cardViewDisplay}</div>
+				</div>
 			</Fragment>
 		);
 	},
 
-	/**
-	 * The save function defines the way in which the different attributes should be combined
-	 * into the final markup, which is then serialized by Gutenberg into post_content.
-	 *
-	 * The "save" property must be specified and must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 *
-	 * @param {Object} props Props.
-	 * @returns {Mixed} JSX Frontend HTML.
-	 */
-	save: props => {
-		var attributes = props.attributes;
+	save: ({ attributes, className, setAttributes }) => {
+		const cardViewDisplay = attributes.cards.map((card, index) => {
+			return <h4 key={index}>{card.title}</h4>;
+		});
 
-		if (attributes.cards.length > 0) {
-			var itemList = attributes.cards.map(function(item) {
-				return <h1>{item.title}</h1>;
-			});
-
-			return <div class="cards">{itemList}</div>;
-		} else {
-			return null;
-		}
+		return (
+			<div className={className + " album album-home text-muted"}>
+				<h2>Cards</h2>
+				{cardViewDisplay}
+			</div>
+		);
 	}
 });
